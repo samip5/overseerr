cd91767ec4ddfcfe307d1102d44583b1a84622ef
Skyler Mäntysaari <sm+git@skym.fi>
2024-01-17 19:42:54 +0200

    feat(db): implement postgres support

--- server/datasource.ts
+++ server/datasource.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import type { DataSourceOptions, EntityTarget, Repository } from 'typeorm';
 import { DataSource } from 'typeorm';
 
-const devConfig: DataSourceOptions = {
+const sqliteDevConfig: DataSourceOptions = {
   type: 'sqlite',
   database: process.env.CONFIG_DIRECTORY
     ? `${process.env.CONFIG_DIRECTORY}/db/db.sqlite3`
@@ -16,7 +16,7 @@ const devConfig: DataSourceOptions = {
   subscribers: ['server/subscriber/**/*.ts'],
 };
 
-const prodConfig: DataSourceOptions = {
+const sqliteProdConfig: DataSourceOptions = {
   type: 'sqlite',
   database: process.env.CONFIG_DIRECTORY
     ? `${process.env.CONFIG_DIRECTORY}/db/db.sqlite3`
@@ -30,8 +30,45 @@ const prodConfig: DataSourceOptions = {
   subscribers: ['dist/subscriber/**/*.js'],
 };
 
+const postgresProdConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  migrationsRun: true,
+  logging: false,
+  entities: ['dist/entity/**/*.js'],
+  migrations: ['dist/migration/**/*.js'],
+  subscribers: ['dist/subscriber/**/*.js'],
+};
+const postgresDevConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  migrationsRun: false,
+  logging: false,
+  entities: ['dist/entity/**/*.js'],
+  migrations: ['dist/migration/**/*.js'],
+  subscribers: ['dist/subscriber/**/*.js'],
+};
+
+// In production, if DB_TYPE is 'postgres', it uses postgresProdConfig; otherwise, it defaults to sqliteProdConfig.
+// In development (NODE_ENV !== 'production'), if DB_TYPE is 'postgres', it uses postgresDevConfig; otherwise, it defaults to sqliteDevConfig.
 const dataSource = new DataSource(
-  process.env.NODE_ENV !== 'production' ? devConfig : prodConfig
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_TYPE === 'postgres'
+      ? postgresProdConfig
+      : sqliteProdConfig
+    : process.env.DB_TYPE === 'postgres'
+    ? postgresDevConfig
+    : sqliteDevConfig
 );
 
 export const getRepository = <Entity extends object>(
